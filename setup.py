481b607ed1901c9283ad1a8dde643837e7a3914c
盧露 <low.lucyy@gmail.com>
2022-05-21 12:49:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
+    install_requires=[
+        'numpy>=1.15',
+        'pandas>=0.23',
+        'scipy>=1.1.0',
+        'torch==1.0.1',
+        'molsets==0.1.3'
+    ],    
     version='0.1.0',
     description='ai drug discovery for technica',
     author='lucy',
